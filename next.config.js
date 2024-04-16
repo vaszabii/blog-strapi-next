8d46e5b5b5ee2571524334a93a46a37670d8739e
Varga Szabolcs <varga2szabolcs2@gmail.com>
2024-04-16 14:19:18 +0200

    initial commit

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  images: {
+    loader: "default",
+    domains: ["https://blog-strapi-postgress.onrender.com"],
+  },
+}
+
+module.exports = nextConfig
