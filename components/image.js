b335fb1f109f180bd5214fad32d2a7ed14be603c
Varga Szabolcs <varga2szabolcs2@gmail.com>
2024-04-16 14:19:46 +0200

    comment out image handeling because demo

--- components/image.js
+++ components/image.js
@@ -2,22 +2,23 @@ import { getStrapiMedia } from "../lib/media"
 import NextImage from "next/image"
 
 const Image = ({ image, style }) => {
-  const { url, alternativeText, width, height } = image.data.attributes
+  // const { url, alternativeText, width, height } = image?.data?.attributes
 
   // const loader = () => {
   //   return getStrapiMedia(image)
   // }
 
   return (
-    <NextImage
-      // loader={loader}
-      layout="responsive"
-      width={width || "100%"}
-      height={height || "100%"}
-      objectFit="contain"
-      src={getStrapiMedia(image)}
-      alt={alternativeText || ""}
-    />
+    // <NextImage
+    //   // loader={loader}
+    //   layout="responsive"
+    //   width={width || "100%"}
+    //   height={height || "100%"}
+    //   objectFit="contain"
+    //   src={getStrapiMedia(image)}
+    //   alt={alternativeText || ""}
+    // />
+    <>Image</>
   )
 }
 
